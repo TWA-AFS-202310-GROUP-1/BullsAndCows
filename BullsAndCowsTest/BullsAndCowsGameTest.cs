432cffee369b0b06ec8d058f659d8214e1bfd8b6
Han Xudong <xhan14@slb.com>
2023-11-01 17:58:53 +0800

    test: add test for case '0A4B'

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -84,5 +84,24 @@ namespace BullsAndCowsTest
             //then
             Assert.Equal("0A0B", result);
         }
+
+        [Theory]
+        [InlineData("2341")]
+        public void Should_return_0A4B_when_guess_given_all_values_correct_and_all_positions_incorrect(string guessNumber)
+        {
+            //given
+            string secret = "1234";
+
+            Mock<SecretGenerator> mockedSecretGenerator = new Mock<SecretGenerator>();
+            mockedSecretGenerator.Setup(x => x.GenerateSecret()).Returns(secret);
+
+            var game = new BullsAndCowsGame(mockedSecretGenerator.Object);
+
+            //when
+            string result = game.Guess(guessNumber);
+
+            //then
+            Assert.Equal("0A4B", result);
+        }
     }
 }
