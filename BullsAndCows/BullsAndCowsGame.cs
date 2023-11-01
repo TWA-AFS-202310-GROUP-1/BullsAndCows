7ac77f5d6ec8be77a363705a455021532b97f503
Han Xudong <xhan14@slb.com>
2023-11-01 17:09:37 +0800

    feat: return 4A0B when answer is correct

--- BullsAndCows/BullsAndCowsGame.cs
+++ BullsAndCows/BullsAndCowsGame.cs
@@ -5,16 +5,25 @@ namespace BullsAndCows
     public class BullsAndCowsGame
     {
         private readonly SecretGenerator secretGenerator;
+        private readonly string secret;
         public BullsAndCowsGame(SecretGenerator secretGenerator)
         {
             this.secretGenerator = secretGenerator;
+            this.secret = this.secretGenerator.GenerateSecret();
         }
 
         public bool CanContinue => true;
 
         public string Guess(string guess)
         {
-            throw new NotImplementedException();
+            if (secret == guess)
+            {
+                return "4A0B";
+            }
+            else
+            {
+                return string.Empty;
+            }
         }
     }
 }
