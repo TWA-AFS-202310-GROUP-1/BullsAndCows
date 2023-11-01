06e7968dffcfbb967a230b5500ea1fb3b7109b77
Yiyi Han <YHan18@slb.com>
2023-11-01 17:03:58 +0800

    test: create test case for 2A0B

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -29,5 +29,22 @@ namespace BullsAndCowsTest
             //Then
             Assert.Equal("4A0B", result);
         }
+
+        [Fact]
+        public void Should_return_2A0B_when_guess_position_and_digit_partial_right()
+        {
+            //Given
+            string guessNumber = "1256";
+            string secretNumber = "1234";
+            Mock<SecretGenerator> mockedSecretGenerator = new Mock<SecretGenerator>();
+            mockedSecretGenerator.Setup(generator => generator.GenerateSecret()).Returns(secretNumber);
+            var game = new BullsAndCowsGame(mockedSecretGenerator.Object);
+            //When
+            string result = game.Guess(guessNumber);
+            //Then
+            Assert.Equal("2A0B", result);
+        }
+
     }
+
 }
