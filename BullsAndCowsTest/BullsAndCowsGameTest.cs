bf7cb972d7785041fbd183a9666e42fd42956f13
JWei9 <JWei9@slb.com>
2023-11-01 16:56:39 +0800

    test: add test for 4A0B

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -1,4 +1,5 @@
 using BullsAndCows;
+using Moq;
 using Xunit;
 
 namespace BullsAndCowsTest
@@ -6,12 +7,20 @@ namespace BullsAndCowsTest
     public class BullsAndCowsGameTest
     {
         [Fact]
-        public void Should_create_BullsAndCowsGame()
+        public void Should_return_4A0B_when_guess_given_guess_number_and_secret_are_same()
         {
-            var secretGenerator = new SecretGenerator();
-            var game = new BullsAndCowsGame(secretGenerator);
-            Assert.NotNull(game);
-            Assert.True(game.CanContinue);
+            //given
+            string guessNumber = "1234";
+            string secret = "1234";
+
+            Mock<SecretGenerator> mockSecretGenerator = new Mock<SecretGenerator>();
+            mockSecretGenerator.Setup(generator => generator.GenerateSecret()).Returns(secret);
+
+            var game = new BullsAndCowsGame(mockSecretGenerator.Object);
+            //when
+            string result = game.Guess(guessNumber);
+            //then
+            Assert.Equal("4A0B", result);
         }
     }
 }
