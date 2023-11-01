ba3eb1b415e47cb6895e36be97221de9449654a9
JWei9 <JWei9@slb.com>
2023-11-01 17:22:59 +0800

    test: update test case for 2A0B by InlineData

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -23,12 +23,14 @@ namespace BullsAndCowsTest
             Assert.Equal("4A0B", result);
         }
 
-        [Fact]
-        public void Should_return_2A0B_when_guess_given_position_and_digit_are_partially_correct()
+        [Theory]
+        [InlineData("1256")]
+        [InlineData("5634")]
+        [InlineData("5239")]
+        public void Should_return_2A0B_when_guess_given_position_and_digit_are_partially_correct(string guessNumber)
         {
             //given
-            string guessNumber = "1234";
-            string secret = "1256";
+            string secret = "1234";
 
             Mock<SecretGenerator> mockSecretGenerator = new Mock<SecretGenerator>();
             mockSecretGenerator.Setup(generator => generator.GenerateSecret()).Returns(secret);
