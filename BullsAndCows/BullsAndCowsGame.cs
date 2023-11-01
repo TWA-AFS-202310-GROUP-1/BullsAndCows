d946728a36ad438c93c9bf9c5d8f78dd4c2ea704
Wency3 <XWen3@slb.com>
2023-11-01 16:51:52 +0800

    feat: add function of 4A0B

--- BullsAndCows/BullsAndCowsGame.cs
+++ BullsAndCows/BullsAndCowsGame.cs
@@ -4,17 +4,24 @@ namespace BullsAndCows
 {
     public class BullsAndCowsGame
     {
+        private readonly string secret;
         private readonly SecretGenerator secretGenerator;
         public BullsAndCowsGame(SecretGenerator secretGenerator)
         {
             this.secretGenerator = secretGenerator;
+            this.secret = secretGenerator.GenerateSecret();
         }
 
         public bool CanContinue => true;
 
         public string Guess(string guess)
         {
-            throw new NotImplementedException();
+            if (guess.Equals(secret))
+            {
+                return "4A0B";
+            }
+
+            return string.Empty;
         }
     }
 }
