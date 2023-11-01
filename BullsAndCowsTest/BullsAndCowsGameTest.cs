2551afe3725c5bba550cd891d2cc8f9f36210545
Yiyi Han <YHan18@slb.com>
2023-11-01 17:35:53 +0800

    test: add a test for return 1A1B when guess position and value partial correct

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -6,20 +6,20 @@ namespace BullsAndCowsTest
 {
     public class BullsAndCowsGameTest
     {
-        [Fact]
-        public void Should_create_BullsAndCowsGame()
-        {
-            var secretGenerator = new SecretGenerator();
-            var game = new BullsAndCowsGame(secretGenerator);
-            Assert.NotNull(game);
-            Assert.True(game.CanContinue);
-        }
+        //[Fact]
+        //public void Should_create_BullsAndCowsGame()
+        //{
+        //    var secretGenerator = new SecretGenerator();
+        //    var game = new BullsAndCowsGame(secretGenerator);
+        //    Assert.NotNull(game);
+        //    Assert.True(game.CanContinue);
+        //}
 
-        [Fact]
-        public void Should_return_4A0B_when_guess_number_and_secret_are_same()
+        [Theory]
+        [InlineData("1234")]
+        public void Should_return_4A0B_when_guess_number_and_secret_are_same(string guessNumber)
         {
             //Given
-            string guessNumber = "1234";
             string secretNumber = "1234";
             Mock<SecretGenerator> mockedSecretGenerator = new Mock<SecretGenerator>();
             mockedSecretGenerator.Setup(generator => generator.GenerateSecret()).Returns(secretNumber);
@@ -30,11 +30,14 @@ namespace BullsAndCowsTest
             Assert.Equal("4A0B", result);
         }
 
-        [Fact]
-        public void Should_return_2A0B_when_guess_position_and_digit_partial_right()
+        [Theory]
+        [InlineData("1256")]
+        [InlineData("5634")]
+        [InlineData("5236")]
+        [InlineData("1564")]
+        public void Should_return_2A0B_when_guess_position_and_digit_partial_right(string guessNumber)
         {
             //Given
-            string guessNumber = "1256";
             string secretNumber = "1234";
             Mock<SecretGenerator> mockedSecretGenerator = new Mock<SecretGenerator>();
             mockedSecretGenerator.Setup(generator => generator.GenerateSecret()).Returns(secretNumber);
@@ -45,6 +48,19 @@ namespace BullsAndCowsTest
             Assert.Equal("2A0B", result);
         }
 
+        [Theory]
+        [InlineData("1562")]
+        public void Should_return_1A1B_when_guess_position_and_value_partial_correct(string guessNumber)
+        {
+            //Given
+            string secretNumber = "1234";
+            Mock<SecretGenerator> mockedSecretGenerator = new Mock<SecretGenerator>();
+            mockedSecretGenerator.Setup(generator => generator.GenerateSecret()).Returns(secretNumber);
+            var game = new BullsAndCowsGame(mockedSecretGenerator.Object);
+            //When
+            string result = game.Guess(guessNumber);
+            //Then
+            Assert.Equal("1A1B", result);
+        }
     }
-
 }
