20e333ba39a1875fa8c74bc3e5e15a79c9603409
Wency3 <XWen3@slb.com>
2023-11-01 17:50:26 +0800

    test: return 2A2B when guess given number and secret are partial right

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -6,15 +6,6 @@ namespace BullsAndCowsTest
 {
     public class BullsAndCowsGameTest
     {
-/*        [Fact]
-        public void Should_create_BullsAndCowsGame()
-        {
-            var secretGenerator = new SecretGenerator();
-            var game = new BullsAndCowsGame(secretGenerator);
-            Assert.NotNull(game);
-            Assert.True(game.CanContinue);
-        }*/
-
         [Fact]
         public void Should_return_4A0B_when_guess_given_guess_number_and_secret_are_same()
         {
@@ -80,7 +71,7 @@ namespace BullsAndCowsTest
 
         [Theory]
         [InlineData("4321")]
-        public void Should_return_0A4B_when_guess_given_guess_number_and_secret_are_partial_right(string guessNumber)
+        public void Should_return_0A4B_when_guess_given_guess_number_and_secret_are_all_right_and_position_all_wrong(string guessNumber)
         {
             //Given
             string secret = "1234";
@@ -91,6 +82,21 @@ namespace BullsAndCowsTest
             string result = game.Guess(guessNumber);
             //Then
             Assert.Equal("0A4B", result);
+        }
+
+        [Theory]
+        [InlineData("1243")]
+        public void Should_return_2A2B_when_guess_given_guess_number_and_secret_are_partial_right(string guessNumber)
+        {
+            //Given
+            string secret = "1234";
+            Mock<SecretGenerator> mockSecretGenerator = new Mock<SecretGenerator>();
+            mockSecretGenerator.Setup(generator => generator.GenerateSecret()).Returns(secret);
+            var game = new BullsAndCowsGame(mockSecretGenerator.Object);
+            //When
+            string result = game.Guess(guessNumber);
+            //Then
+            Assert.Equal("2A2B", result);
         }
     }
 }
