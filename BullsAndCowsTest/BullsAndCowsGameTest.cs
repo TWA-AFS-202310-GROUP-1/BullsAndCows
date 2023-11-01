e5dcf6f4b5c76c9a26aa24d8d286b19f40b32e6b
Xinxin Xu <xxu37@slb.com>
2023-11-01 17:49:42 +0800

    test: add test of all digit and position incorrect

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -65,5 +65,24 @@ namespace BullsAndCowsTest
             // Then
             Assert.Equal("1A1B", result);
         }
+
+        [Theory]
+        [InlineData("5678")]
+        public void Should_0A0B_when_guess_given_all_digit_and_position_incorrect(string guessNumber)
+        {
+            // Given
+            string secret = "1234";
+
+            Mock<SecretGenerator> mockedSecretGenerator = new Mock<SecretGenerator>();
+            mockedSecretGenerator.Setup(generator => generator.GenerateSecret()).Returns(secret);
+
+            var game = new BullsAndCowsGame(mockedSecretGenerator.Object);
+
+            // When
+            string result = game.Guess(guessNumber);
+
+            // Then
+            Assert.Equal("0A0B", result);
+        }
     }
 }
